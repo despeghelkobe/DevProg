2d882025c5a53bb02d86fa2fe5ea933f5abf6c95
despeghelkobe <kobe.despeghel@student.howest.be>
2022-09-30 11:33:22 +0200

    commit week02

---
+++ demow02/demow02/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using demow02.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace demow02
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Beer b = new Beer("jupiler", "AB inbev", 5.2, "blond");
+            Debug.WriteLine(b);
+
+        }
+    }
+}
---
+++ demow02/demow02/Models/Beer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace demow02.Models
+{
+    public class Beer
+    {
+        private string _brewery;
+        private string _name;
+        private string _fullName;
+
+        public Guid BeerId { get; set; }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value != null)
+                    _name = value;
+                else
+                    _name = "unknown";
+            }
+        }
+
+        public string Brewery
+        { 
+            get => _brewery;
+            set
+            {
+                if (_brewery != null)
+                    _brewery = value;
+                else
+                    _brewery = "unknown";
+            }
+        }
+
+        public double Alcohol { get; set; }
+
+        public string Color { get; set; }
+
+        public string FullName
+        {
+            get => $"{Name} ({Brewery})";
+        }
+
+
+        //ctors
+
+        public Beer(string name, string brewery, double alcohol, string color)
+        {
+            BeerId = Guid.NewGuid();
+            Name = name;
+            Brewery = brewery;
+            Alcohol = alcohol;
+            Color = color;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {FullName}, Alcohol percentage: {Alcohol}%, Color: {Color}";
+        }
+    }
+}
